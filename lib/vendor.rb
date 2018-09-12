b0fab79d65435846de4e5e3b45a194f678af8eed
Michael Gatewood <michael@mngatewood.com>
2018-09-12 13:20:51 -0600

    Adds vendor and tests

---
+++ lib/vendor.rb
@@ -0,0 +1,19 @@
+class Vendor
+
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(product)
+    return @inventory[product]
+  end
+
+  def stock(product, quantity)
+    @inventory[product] += quantity
+  end
+
+end
