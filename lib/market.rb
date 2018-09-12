96372745aa655cae2ae07cc51da2a3329b56e0e1
Michael Gatewood <michael@mngatewood.com>
2018-09-12 14:54:26 -0600

    Adds sell and reduce stock methods to Market and tests

--- lib/market.rb
+++ lib/market.rb
@@ -15,7 +15,7 @@ class Market
   end
 
   def vendor_names
-    return @vendors.map{|vendor|vendor.name}
+    return vendors.map{|vendor|vendor.name}
   end
 
   def vendors_that_sell(product)
@@ -38,6 +38,29 @@ class Market
       end
     end
     return total_inventory
+  end
+
+  def sell(product, quantity)
+    market_stock = total_inventory[product]
+    if market_stock < quantity
+      return false
+    else
+      reduce_stock(product, quantity)
+    end
+  end
+
+  def reduce_stock(product, initial_quantity)
+    quantity = initial_quantity
+    vendors.each do |vendor|
+      vendor_stock = vendor.inventory[product]
+      if vendor_stock >= quantity
+        vendor.sell(product, quantity)
+        return true
+      elsif vendor_stock > 0
+        vendor.sell(product, vendor_stock)
+        quantity -= vendor_stock
+      end
+    end    
   end
 
 end
